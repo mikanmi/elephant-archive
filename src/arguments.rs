80a0c4510ba11c605f4a90452416fceb0d5e069d
Patineboot <patine@patineboot.com>
2022-10-23 15:31:51 +0900

    [ADD] the snapshot-list sub-command.

---
+++ src/arguments.rs
@@ -0,0 +1,70 @@
+// Copyright (c) 2022 Patineboot.
+// All rights reserved.
+// Elephant Archive is licensed under BSD 2-Clause License.
+
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+pub struct Arguments;
+
+impl Arguments {
+    pub fn get_options() -> String {
+        let cli = Cli::parse();
+
+        String::from("aaa")
+    }
+}
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Optional name to operate on
+    name: Option<String>,
+
+    /// Sets a custom config file
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<PathBuf>,
+
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    /// TODO: implementation
+    Archive {
+        /// lists test values
+        #[arg(short, long)]
+        list: bool,
+    },
+    /// TODO: implementation
+    Restore {
+        /// lists test values
+        #[arg(short, long)]
+        list: bool,
+    },
+    /// TODO: implementation
+    Diff {
+        /// lists test values
+        #[arg(short, long)]
+        list: bool,
+    },
+    /// Take a snapshot and purge some existing snapshots on a ZFS filesystem.
+    Snapshot {
+        /// lists test values
+        #[arg(short, long)]
+        list: bool,
+    },
+    /// TODO: implementation
+    ListSnapshot {
+        /// lists test values
+        #[arg(short, long)]
+        list: bool,
+    },
+}
+
--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,31 @@
 // All rights reserved.
 // Elephant Archive is licensed under BSD 2-Clause License.
 
-use log::{error, warn, info, debug, trace};
-
+mod arguments;
 mod elephant_logger;
 mod zfs;
 
 use zfs::Filesystem;
 
 fn main() {
-
     elephant_logger::ElephantLogger::init().unwrap();
+
+    let cli = arguments::Arguments::get_options();
+
 
     let number = 9;
 
     println!("Hello, world!{}", number);
 
-    error!("log error message");
-    warn!("log warn message");
-    info!("log info message");
-    debug!("log debug message");
-    trace!("log trace message");
+    log::error!("log error message");
+    log::warn!("log warn message");
+    log::info!("log info message");
+    log::debug!("log debug message");
+    log::trace!("log trace message");
 
     archive();
 
-    info!("Finished Elephant Archive.");
+    log::info!("Finished Elephant Archive.");
 }
 
 fn archive() {
@@ -34,6 +35,6 @@ fn archive() {
     let filesystem = Filesystem::new(pool_name);
     let snapshots = filesystem.get_snapshots();
 
-    info!("Snapshots: {:?}", snapshots);
-    info!("Snapshots: {:?}", snapshots[0]);
+    log::info!("Snapshots: {:?}", snapshots);
+    log::info!("Snapshots: {:?}", snapshots[0]);
 }
